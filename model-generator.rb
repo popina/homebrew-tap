6bc730e085c016fcd9f9c7118f925e1548799383
mastohhh <mastohhh@users.noreply.github.com>
2020-11-05 08:45:13 +0100

    Update model-generator.rb

--- model-generator.rb
+++ model-generator.rb
@@ -5,7 +5,7 @@ class ModelGenerator < Formula
   desc "Popina model generator"
   homepage "https://github.com/popina/model-generator"
   version "0.0.6"
-  url "git@github.com:popina/model-generator.git", :tag => "#{version}"
+  url "https://github.com/popina/model-generator.git", :tag => "#{version}"
   depends_on :xcode => ["11.2.1", :build]
 
   def install
